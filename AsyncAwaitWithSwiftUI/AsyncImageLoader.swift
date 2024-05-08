231367258eb003afb747bb24d0ecf4894590283f
crisine <crisine@knou.ac.kr>
2024-05-08 23:12:43 +0900

    feat: 비동기 통신을 통해 이미지를 받아오는 AsyncImageLoader 클래스 구현 - URLSession과 .dataTask() 메서드를 사용하여 이미지를 로드하는 urlSessionImageLoad() 클래스 구현 - 이미지를 불러오는 도중 발생할 수 있는 에러 처리를 위한 ImageLoadError enum 구현 - SkeletonUI를 통해 이미지가 로드되는 중임을 표시하도록 구현

---
+++ AsyncAwaitWithSwiftUI/AsyncImageLoader.swift
@@ -0,0 +1,84 @@
+//
+//  AsyncImageLoader.swift
+//  AsyncAwaitWithSwiftUI
+//
+//  Created by Minho on 5/8/24.
+//
+
+import Foundation
+import UIKit
+
+enum ImageLoadError: Error {
+    case invalidUrl
+    case invalidResponse
+    case invalidImage
+    case unknown
+    
+    var description: String {
+        switch self {
+        case .invalidUrl:
+            return "유효하지 않은 URL이 입력되었습니다."
+        case .invalidResponse:
+            return "유효하지 않은 응답입니다."
+        case .invalidImage:
+            return "올바르지 않은 이미지입니다."
+        case .unknown:
+            return "알 수 없는 에러입니다."
+        }
+    }
+}
+
+final class AsyncImageLoader {
+    
+    static let shared = AsyncImageLoader()
+    
+    private init() {}
+    
+    // URLSession 사용하여 이미지 로드
+    func urlSessionImageLoad(imageUrl: URL?, completion: @escaping (Result<UIImage, ImageLoadError>) -> Void) throws {
+        
+        // 유효하지 않은 URL을 입력한 경우 .invalidUrl 에러를 클로저로 넘기고 조기 종료
+        guard let imageUrl else {
+            completion(.failure(.invalidUrl))
+            return
+        }
+        
+        /*
+         useProtocolCachePolicy: protcol 특성에 따른 기본 캐시정책 (서버에서 전달한 Cache-Control 헤더 그대로 따름)
+         reloadIgnoringLocalCacheData : local 캐시를 무시하고 항상 네트워크에 접속하도록 설정하는 정책
+         returnCacheDataDontLoad : 네트워크에 접속하지 않고 항상 local 캐시를 사용하도록 설정하는 정책
+         returnCacheDataElseLoad : local 캐시를 확인하고 캐시가 없는 경우에만 네트워크에 접속하도록 설정하는 정책
+         */
+        let request = URLRequest(url: imageUrl,
+                                 cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                                 timeoutInterval: 5)
+        
+        // DataTask: HTTP의 각종 메서드를 이용해 서버로부터 응답을 받아 Data객체를 가져오는 작업 수행
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            guard let data else {
+                completion(.failure(.unknown))
+                return
+            }
+            
+            guard error == nil else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+                completion(.failure(.invalidImage))
+                return
+            }
+            
+            completion(.success(image))
+        }.resume()
+        
+    }
+}
--- AsyncAwaitWithSwiftUI/ContentView.swift
+++ AsyncAwaitWithSwiftUI/ContentView.swift
@@ -6,16 +6,48 @@
 //
 
 import SwiftUI
+import SkeletonUI
 
 struct ContentView: View {
+    
+    let imageUrl = URL(string: "https://picsum.phoos/200")
+    @State var isImageLoading = false
+    @State var loadedImage: UIImage?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(uiImage: loadedImage ?? UIImage(systemName: "photo")!)
+                .skeleton(with: isImageLoading,
+                          shape: .rectangle)
+                .frame(width: 200, height: 200)
+            Button("이미지 로딩") {
+                isImageLoading = true
+                asyncImageLoad()
+            }
+            .foregroundStyle(.white)
+            .padding(8)
+            .background(.blue)
+            .clipShape(.buttonBorder)
+            
         }
         .padding()
+    }
+    
+    func asyncImageLoad() {
+        do {
+            try AsyncImageLoader.shared.urlSessionImageLoad(imageUrl: imageUrl) { response in
+                switch response {
+                case .success(let image):
+                    loadedImage = image
+                    isImageLoading = false
+                case .failure(let error):
+                    print(error)
+                    isImageLoading = false
+                }
+            }
+        } catch {
+            print(error)
+        }
     }
 }
 
