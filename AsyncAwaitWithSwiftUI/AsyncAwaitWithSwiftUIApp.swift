45361b7812ebe95ac021479e2515d91288cd4322
crisine <crisine@knou.ac.kr>
2024-05-08 20:37:39 +0900

    init: 프로젝트 초기 커밋

---
+++ AsyncAwaitWithSwiftUI/AsyncAwaitWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitWithSwiftUIApp.swift
+//  AsyncAwaitWithSwiftUI
+//
+//  Created by Minho on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
