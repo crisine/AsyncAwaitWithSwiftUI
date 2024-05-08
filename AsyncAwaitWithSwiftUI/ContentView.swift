f25b2218734ad9aa3cf7dad3af6417f086ae95e9
crisine <crisine@knou.ac.kr>
2024-05-08 23:20:39 +0900

    feat: 에러 상황이 발생했을때 toast를 통해 에러 메시지를 보여줄 수 있도록 구현

--- AsyncAwaitWithSwiftUI/ContentView.swift
+++ AsyncAwaitWithSwiftUI/ContentView.swift
@@ -7,12 +7,15 @@
 
 import SwiftUI
 import SkeletonUI
+import AlertToast
 
 struct ContentView: View {
     
-    let imageUrl = URL(string: "https://picsum.phoos/200")
-    @State var isImageLoading = false
+    let imageUrl = URL(string: "https://picsum.photos/200")
     @State var loadedImage: UIImage?
+    @State var isImageLoading = false
+    @State var showToast = false
+    @State var errorMessage = ""
     
     var body: some View {
         VStack {
@@ -31,6 +34,10 @@ struct ContentView: View {
             
         }
         .padding()
+        .toast(isPresenting: $showToast) {
+            AlertToast(displayMode: .banner(.pop),
+                       type: .error(.red), title: errorMessage)
+        }
     }
     
     func asyncImageLoad() {
@@ -42,6 +49,8 @@ struct ContentView: View {
                     isImageLoading = false
                 case .failure(let error):
                     print(error)
+                    errorMessage = error.description
+                    showToast = true
                     isImageLoading = false
                 }
             }
